f34d99919c32e28d49ae32beba4eb108b3200d5f
parkermorgan <parker.morgan.te@gmail.com>
2024-07-20 17:04:58 -0600

    added review page

--- project/scripts/review.js
+++ project/scripts/review.js
@@ -12,16 +12,7 @@ const products = [
       name: "Home",
       avgRating: 4.5
     },
-    {
-      id: 'fc-2050',
-      name: "Notable Characters",
-      avgRating: 4.7
-    },
-    {
-      id: 'fs-1987',
-      name: "Episode Guide",
-      avgRating: 3.5
-    },
+
     {
       id: 'ac-2000',
       name: "Gallery",
